869ae04f91663234806f6f3598e9442d24ab2c3a
ekety <52764025+ekety@users.noreply.github.com>
2019-10-03 01:30:17 +0300

    Add files via upload

---
+++ DateService/js/script.js
@@ -0,0 +1,22 @@
+$('.statics1').click(function() {
+	$('.info1').slideToggle();
+});
+
+$('.statics2').click(function() {
+	$('.info2').slideToggle();
+});
+
+$('.statics3').click(function() {
+	$('.info3').slideToggle();
+});
+
+$('.statics4').click(function() {
+	$('.info4').slideToggle();
+});
+
+$('.statics5').click(function() {
+	$('.info5').slideToggle();
+});
+
+
+
