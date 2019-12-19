3e87f9d1f2f80a31cdb49c166f6ca855d2c310df
ekety <52764025+ekety@users.noreply.github.com>
2019-12-19 23:24:05 +0300

    Add files via upload

--- Minimo/js/jQueryscript.js
+++ Minimo/js/jQueryscript.js
@@ -1,23 +1,3 @@
-$(function () {
-		$('.owl-carousel').owlCarousel({
-		loop: true,
-		margin: 10,
-		nav: true,
-		navText: ['<img src="img/prev.png" alt="">', '<img src="img/next.png" alt="">'],
-		responsive: {
-			0: {
-				items: 1
-			},
-			600: {
-				items: 1
-			},
-			1000: {
-				items: 1
-			}
-		}
-	});
-	
-});
 
 $(function () {
 	
@@ -45,3 +25,23 @@ $(function () {
 });
 
 
+$(function () {
+		$('.owl-carousel').owlCarousel({
+		loop: true,
+		margin: 10,
+		nav: true,
+		navText: ['<img src="img/prev.png" alt="">', '<img src="img/next.png" alt="">'],
+		responsive: {
+			0: {
+				items: 1
+			},
+			600: {
+				items: 1
+			},
+			1000: {
+				items: 1
+			}
+		}
+	});
+	
+});
