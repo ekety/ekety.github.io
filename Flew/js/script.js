6d4824a85fb082c4dbe55d91fac8b758d7a4cf8a
ekety <52764025+ekety@users.noreply.github.com>
2019-12-26 00:06:11 +0300

    Add files via upload

--- Flew/js/script.js
+++ Flew/js/script.js
@@ -85,5 +85,19 @@ $(function () {
 
 	};
 
+	let link = function() {
+		let body = $("body");
+		body.fadeIn(400);
+		$(document).on("click", "a:not([href='#']):not([href='tel']):not([href='mailto'])", function(e) {
+			e.preventDefault();
+			$("body").fadeOut(400);
+			let self = this;
+			setTimeout(function() {
+				window.location.href = $(self).attr("href");
+			}, 400)
+		});
+	}
+	
 	main();
+	link();
 });
