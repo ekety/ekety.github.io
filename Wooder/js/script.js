c02d8cca44540c78b277a46cf0d0bfbe48d3f0ff
ekety <52764025+ekety@users.noreply.github.com>
2020-01-16 23:39:03 +0300

    Add files via upload

---
+++ Wooder/js/script.js
@@ -0,0 +1,27 @@
+$(function () {
+    $('.header__slider').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: false,
+        arrows: false,
+        centerMode: false,
+        asNavFor: '.slider__dots',
+    });
+
+    $('.slider__dots').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        asNavFor: '.header__slider',
+        dots: false,
+        arrows: false,
+        centerMode: true,
+        focusOnSelect: true,
+        vertical: false,
+    });
+
+    $('.header__hamburger-menu').on('click', function() {
+        $('.menu').toggleClass('active');
+    })
+
+});
