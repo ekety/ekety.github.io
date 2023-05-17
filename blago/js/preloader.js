ee7bc2ef94f081d2914bd09d504656cda09c3ce6
ekety <52764025+ekety@users.noreply.github.com>
2023-05-17 18:52:26 +0300

    Delete blago directory

--- blago/js/preloader.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    let preloader = document.getElementsByClassName('preloader');
-    setTimeout(function () {
-        preloader[0].classList.add('done');
-    }, 700);
-});
